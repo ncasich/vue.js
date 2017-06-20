9d6c9f667d1e750359e4e3e64a6597181a66cfe5
N. Casich <nickolay.casich@gmail.com>
2017-06-20 13:25:21 +0300

    ## About This Project

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,5 +18,77 @@ Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
     el: '#app',
-    data: {title: 'Hello, Vue.JS!'},
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        turns: []
+    },
+    methods: {
+        startGame(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+        attack(){
+            if (!this.playerAttack(3, 10)) {
+                this.monsterAttack();
+            }
+        },
+        specialAttack(){
+            if (!this.playerAttack(10, 20)) {
+                this.monsterAttack();
+            }
+        },
+        playerAttack(min, max){
+            let damage = this.calculateDamage(min, max);
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+            this.monsterHealth -= damage;
+            return this.checkWin();
+        },
+        monsterAttack(){
+            let damage = this.calculateDamage(5, 12);
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
+            this.playerHealth -= damage;
+            this.checkWin();
+        },
+        heal(){
+            this.playerHealth += 10;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+            this.monsterAttack();
+        },
+        giveUp(){
+            this.gameIsRunning = false;
+        },
+        calculateDamage(min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin(){
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                    return true;
+                }
+                this.gameIsRunning = false;
+                return false;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                    return true;
+                }
+                this.gameIsRunning = false;
+                return false;
+            }
+        },
+    },
 });
