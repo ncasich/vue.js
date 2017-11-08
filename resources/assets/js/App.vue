b625b84eccaf82a1c7c6c8b4f4b8067ea55e10a1
N. Casich <nickolay.casich@gmail.com>
2017-11-08 14:53:17 +0200

    quote part with slots working

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<button @click="selectedComponent = 'appQuote'">Quote</button>
+				<button @click="selectedComponent = 'appAuthor'">Author</button>
+				<button @click="selectedComponent = 'appNew'">New</button>
+				
+				<hr>
+				<app-quote>
+					<div class="quote-title" slot="title">{{quoteTitle}}</div>
+					<div class="quote-subtitle" slot="subtitle">{{quoteSubtitle}}</div>
+					<div class="quote-content">{{quoteContent}}</div>
+				</app-quote>
+				
+				<component :is="selectedComponent">
+					<!--<div slot="title">{{quoteTitle}}</div>-->
+					<!--<div slot="subtitle">{{quoteSubtitle}}</div>-->
+					<!--<div>{{quoteContent}}</div>-->
+				</component>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Quote from './components/Quote.vue';
+	import Author from './components/Author.vue';
+	import New from './components/New.vue';
+	
+	export default {
+		data(){
+			return {
+				quoteTitle: 'The Quote',
+				quoteContent: 'That\'s a wonderful quote!',
+				quoteSubtitle: 'The Subtitle',
+				selectedComponent: 'appQuote'
+			};
+		},
+		components: {
+			appQuote: Quote,
+			appAuthor: Author,
+			appNew: New,
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Quote.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="quote">
+		<p class="default-title">
+			<slot name="title">{{quoteTitle}}</slot>
+		</p>
+		<p class="default-subtitle"><slot name="subtitle">{{quoteSubTitle}}</slot></p>
+		<hr>
+		<p class="default-content"><slot>{{quoteContent}}</slot></p>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				quoteTitle: 'This is from quote Title',
+				quoteSubTitle: 'This is from quote Subtitile',
+				quoteContent: 'This is default slot',
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	div {
+		border: 1px solid #ccc;
+		text-align: center;
+		box-shadow: 1px 1px #000;
+		margin: 30px auto;
+		padding: 30px;
+		font-size: 45px;
+	}
+	
+	.quote-title {
+		color: #f00;
+		font-weight: 900;
+	}
+	
+	.quote-subtitle {
+		color: #163d79;
+		font-weight: 500;
+	}
+	
+	.quote-content {
+		color: #0d3625;
+		font-style: italic;
+	}
+	
+	.default-title {
+		color: rgba(0, 15, 232, 0.85);
+		font-weight: 900;
+	}
+	
+	.default-subtitle {
+		color: #02b203;
+		font-weight: 500;
+	}
+	
+	.default-content {
+		color: #f08600;
+		font-style: italic;
+	}
+
+</style>
