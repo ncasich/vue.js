ca1f573f8433fb5ef094b02a411f6c39dba0605f
N. Casich <nickolay.casich@gmail.com>
2017-11-21 17:31:38 +0200

    finished quotes project

---
+++ resources/assets/js/components/NewQuote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label for="new-quote">Quote</label>
+                <textarea id="new-quote" name="new-quote" class="form-control" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew(){
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
