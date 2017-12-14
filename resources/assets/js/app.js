644c328ee4789366259f3b58c73a06521c5d2826
N. Casich <nickolay.casich@gmail.com>
2017-12-14 16:59:12 +0200

    creating custom directives localy and globaly

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,6 +18,27 @@ import App from './App.vue'
 
 export const eventBus = new Vue();
 
+Vue.directive('highlight', {
+	bind(el, binding, vnode){
+		// el.style.backgroundColor = 'green';
+		// el.style.backgroundColor = binding.value;
+
+		let delay = 0;
+		if(binding.modifiers['delayed']){
+			delay = 5000;
+		}
+
+		setTimeout(()=> {
+            if (binding.arg === 'background') {
+                el.style.backgroundColor = binding.value;
+            } else {
+                el.style.color = binding.value;
+            }
+        }, delay);
+
+	}
+});
+
 new Vue({
 	el: '#content',
 	render: h => h(App)
